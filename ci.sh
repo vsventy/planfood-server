8b91afdb84ba3a4a8e9f41be74bf70d3439668d2
Vitalii Sventyi <vsventy@gmail.com>
2018-09-26 16:30:16 +0300

    add CI script

---
+++ ci.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+pyclean () {
+  # Clean cache:
+  find . | grep -E '(__pycache__|\.py[cod]$)' | xargs rm -rf
+}
+
+run_ci () {
+  # Run tests:
+  mypy planfood
+  py.test
+
+  # Check style:
+  black --check .
+
+  # Check that all migrations worked fine:
+  python manage.py makemigrations --dry-run --check
+}
+
+# Remove any cache before the script:
+pyclean
+
+# Clean everything up:
+trap pyclean EXIT INT TERM
+
+# Run the CI process:
+run_ci
