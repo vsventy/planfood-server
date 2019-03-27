04459ae52b2e7991771cdc5efc29b176d7e8b5e3
Vitalii Sventyi <svitaha@gmail.com>
2019-03-27 15:48:39 +0200

    create norms and outputs of dishes

--- planfood/dishes/admin.py
+++ planfood/dishes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.management import call_command
 from django.utils.translation import ugettext_lazy as _
 
 from .models import Dish, Norm, Output
@@ -6,27 +7,53 @@ from .models import Dish, Norm, Output
 
 class NormInline(admin.TabularInline):
     model = Norm
-    min_num = 2
 
 
 class OutputInline(admin.TabularInline):
     model = Output
-    min_num = 2
 
 
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
     fields = ('name', 'dish_type', 'products')
-    list_display = ('name', 'dish_type', 'products_count', 'norms_count')
+    list_display = (
+        'name',
+        'dish_type',
+        'products_count',
+        'outputs_count',
+        'norms_count',
+    )
     list_filter = ('dish_type',)
     filter_horizontal = ('products',)
     inlines = (OutputInline, NormInline)
+    actions = ['create_norms', 'create_outputs']
+    actions_without_queryset = ['create_norms', 'create_outputs']
 
     def products_count(self, obj):
         return obj.products.all().count()
 
     def norms_count(self, obj):
-        return obj.norms.all().count()
+        return '{} ({})'.format(
+            obj.norms.all().count(), obj.norms.filter(value=0).count()
+        )
+
+    def outputs_count(self, obj):
+        return '{} ({})'.format(
+            obj.outputs.all().count(), obj.outputs.filter(value='0').count()
+        )
+
+    def create_norms(self, request, queryset):
+        call_command('create_norms')
+
+    def create_outputs(self, request, queryset):
+        call_command('create_outputs')
 
     norms_count.short_description = _('Number of Products Norms')  # type: ignore
+    outputs_count.short_description = _('Number of Products Outputs')  # type: ignore
     products_count.short_description = _('Number of Products')  # type: ignore
+    create_norms.short_description = _(
+        'Creates empty norms for all dishes'
+    )  # type: ignore
+    create_outputs.short_description = _(
+        'Creates empty outputs for all dishes'
+    )  # type: ignore
---
+++ planfood/dishes/management/commands/create_outputs.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+
+from planfood.common.models import AgeCategory, Group
+from planfood.dishes.models import Dish, Output
+
+
+class Command(BaseCommand):
+    help = 'Creates empty output items for each dish'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Starting...')
+
+        dishes = Dish.objects.all()
+        groups = Group.objects.all()
+        age_categories = AgeCategory.objects.all()
+
+        for dish in dishes.iterator():
+            for group in groups.iterator():
+                for age_category in age_categories.iterator():
+
+                    has_output = dish.outputs.filter(
+                        group=group, age_category=age_category
+                    ).exists()
+
+                    if not has_output:
+                        output = Output.objects.create(
+                            dish=dish, group=group, age_category=age_category, value='0'
+                        )
+                        self.stdout.write(
+                            '"{} - {} - {} - {}" is added'.format(
+                                output.dish.name,
+                                output.group.name,
+                                output.age_category.name,
+                                output.value,
+                            )
+                        )
+
+        self.stdout.write('Finished.')
